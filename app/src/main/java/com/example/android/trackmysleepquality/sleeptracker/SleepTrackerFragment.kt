fee94f6b6050c8c0b925ee7c1fc2a04e638da954
lumilitas <lucas23121995>
2020-07-16 16:20:10 -0300

    Configure Header to fulfill one row

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -62,6 +62,13 @@ class SleepTrackerFragment : Fragment() {
                 this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object  : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int = when (position) {
+                0 -> 3
+                else -> 1
+            }
+
+        }
         binding.sleepList.layoutManager = manager
 
         binding.setLifecycleOwner(this)
